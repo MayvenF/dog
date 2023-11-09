f4f6176d525be8970f0e2abf87b8ecf48e77c429
Maeven Fanebust <maeven.fanebust@gmail.com>
2023-11-09 14:49:19 +0200

    second commit

---
+++ dog.go
@@ -0,0 +1,9 @@
+package dog
+
+import (
+	"strings"
+)
+
+func whenGrownUp(s string) string {
+	return "When dog grows up it says: " + strings.ToUpper(s)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/MayvenF/dog
+
+go 1.21.3
